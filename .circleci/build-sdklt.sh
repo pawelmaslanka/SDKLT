72b587adc7425f130285bae243aa61ad8fba1ea4
pudelkoM <pudelkoM@users.noreply.github.com>
2020-02-07 11:32:45 -0800

    Downgrade compilers for old debian

--- .circleci/build-sdklt.sh
+++ .circleci/build-sdklt.sh
@@ -27,14 +27,15 @@ cd $SDK/appl/demo
 make TARGET_PLATFORM=xlr_linux clean -j
 make TARGET_PLATFORM=xlr_linux -j 2
 
-TARFOLDER=mktemp -d
+TAR_FOLDER=/tmp/sdklt-${KERNEL_VERSION}
+mkdir -p ${TAR_FOLDER}
 
-cp $SDKLT/src/appl/linux/build/xlr_linux/lkm/knet/linux_ngknet.ko \
-   $SDKLT/src/appl/linux/build/xlr_linux/lkm/bde/linux_ngbde.ko \
-   $SDKLT/src/appl/demo/build/xlr_linux/sdklt \
-   $SDKLT/src/appl/sdklib/build/xlr_linux/include/sdklt \
-   $SDKLT/src/appl/sdklib/build/xlr_linux/lib/libsdklt.a \
-   $SDKLT/src/appl/sdklib/build/xlr_linux/lib/libsdklt.so \
-   ${TARFOLDER}
+cp -r $SDKLT/src/appl/linux/build/xlr_linux/lkm/knet/linux_ngknet.ko \
+      $SDKLT/src/appl/linux/build/xlr_linux/lkm/bde/linux_ngbde.ko \
+      $SDKLT/src/appl/demo/build/xlr_linux/sdklt \
+      $SDKLT/src/appl/sdklib/build/xlr_linux/include \
+      $SDKLT/src/appl/sdklib/build/xlr_linux/lib \
+      ${TAR_FOLDER}
 
-tar czf $HOME/sdklt-${KERNEL_VERSION}.tgz ${TARFOLDER}
+tar czf $HOME/sdklt-${KERNEL_VERSION}.tgz \
+  -C /tmp sdklt-${KERNEL_VERSION}
