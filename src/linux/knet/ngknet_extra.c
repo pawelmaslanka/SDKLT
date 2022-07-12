3863d8a43a405b4efd877cef09c32d3e083977d3
pudelkoM <pudelkoM@users.noreply.github.com>
2022-07-12 15:22:16 -0700

    kernel code fixes

--- src/linux/knet/ngknet_extra.c
+++ src/linux/knet/ngknet_extra.c
@@ -3,89 +3,89 @@
  * Utility routines for NGKNET enhancement.
  */
 /*
- * Copyright: (c) 2018 Broadcom. All Rights Reserved. "Broadcom" refers to 
+ * Copyright: (c) 2018 Broadcom. All Rights Reserved. "Broadcom" refers to
  * Broadcom Limited and/or its subsidiaries.
- * 
+ *
  * Broadcom Switch Software License
- * 
- * This license governs the use of the accompanying Broadcom software. Your 
- * use of the software indicates your acceptance of the terms and conditions 
- * of this license. If you do not agree to the terms and conditions of this 
+ *
+ * This license governs the use of the accompanying Broadcom software. Your
+ * use of the software indicates your acceptance of the terms and conditions
+ * of this license. If you do not agree to the terms and conditions of this
  * license, do not use the software.
  * 1. Definitions
  *    "Licensor" means any person or entity that distributes its Work.
- *    "Software" means the original work of authorship made available under 
+ *    "Software" means the original work of authorship made available under
  *    this license.
- *    "Work" means the Software and any additions to or derivative works of 
+ *    "Work" means the Software and any additions to or derivative works of
  *    the Software that are made available under this license.
- *    The terms "reproduce," "reproduction," "derivative works," and 
+ *    The terms "reproduce," "reproduction," "derivative works," and
  *    "distribution" have the meaning as provided under U.S. copyright law.
- *    Works, including the Software, are "made available" under this license 
- *    by including in or with the Work either (a) a copyright notice 
- *    referencing the applicability of this license to the Work, or (b) a copy 
+ *    Works, including the Software, are "made available" under this license
+ *    by including in or with the Work either (a) a copyright notice
+ *    referencing the applicability of this license to the Work, or (b) a copy
  *    of this license.
  * 2. Grant of Copyright License
- *    Subject to the terms and conditions of this license, each Licensor 
- *    grants to you a perpetual, worldwide, non-exclusive, and royalty-free 
- *    copyright license to reproduce, prepare derivative works of, publicly 
- *    display, publicly perform, sublicense and distribute its Work and any 
+ *    Subject to the terms and conditions of this license, each Licensor
+ *    grants to you a perpetual, worldwide, non-exclusive, and royalty-free
+ *    copyright license to reproduce, prepare derivative works of, publicly
+ *    display, publicly perform, sublicense and distribute its Work and any
  *    resulting derivative works in any form.
  * 3. Grant of Patent License
- *    Subject to the terms and conditions of this license, each Licensor 
- *    grants to you a perpetual, worldwide, non-exclusive, and royalty-free 
- *    patent license to make, have made, use, offer to sell, sell, import, and 
- *    otherwise transfer its Work, in whole or in part. This patent license 
- *    applies only to the patent claims licensable by Licensor that would be 
- *    infringed by Licensor's Work (or portion thereof) individually and 
+ *    Subject to the terms and conditions of this license, each Licensor
+ *    grants to you a perpetual, worldwide, non-exclusive, and royalty-free
+ *    patent license to make, have made, use, offer to sell, sell, import, and
+ *    otherwise transfer its Work, in whole or in part. This patent license
+ *    applies only to the patent claims licensable by Licensor that would be
+ *    infringed by Licensor's Work (or portion thereof) individually and
  *    excluding any combinations with any other materials or technology.
- *    If you institute patent litigation against any Licensor (including a 
- *    cross-claim or counterclaim in a lawsuit) to enforce any patents that 
- *    you allege are infringed by any Work, then your patent license from such 
- *    Licensor to the Work shall terminate as of the date such litigation is 
+ *    If you institute patent litigation against any Licensor (including a
+ *    cross-claim or counterclaim in a lawsuit) to enforce any patents that
+ *    you allege are infringed by any Work, then your patent license from such
+ *    Licensor to the Work shall terminate as of the date such litigation is
  *    filed.
  * 4. Redistribution
- *    You may reproduce or distribute the Work only if (a) you do so under 
- *    this License, (b) you include a complete copy of this License with your 
- *    distribution, and (c) you retain without modification any copyright, 
+ *    You may reproduce or distribute the Work only if (a) you do so under
+ *    this License, (b) you include a complete copy of this License with your
+ *    distribution, and (c) you retain without modification any copyright,
  *    patent, trademark, or attribution notices that are present in the Work.
  * 5. Derivative Works
- *    You may specify that additional or different terms apply to the use, 
- *    reproduction, and distribution of your derivative works of the Work 
- *    ("Your Terms") only if (a) Your Terms provide that the limitations of 
- *    Section 7 apply to your derivative works, and (b) you identify the 
- *    specific derivative works that are subject to Your Terms. 
- *    Notwithstanding Your Terms, this license (including the redistribution 
+ *    You may specify that additional or different terms apply to the use,
+ *    reproduction, and distribution of your derivative works of the Work
+ *    ("Your Terms") only if (a) Your Terms provide that the limitations of
+ *    Section 7 apply to your derivative works, and (b) you identify the
+ *    specific derivative works that are subject to Your Terms.
+ *    Notwithstanding Your Terms, this license (including the redistribution
  *    requirements in Section 4) will continue to apply to the Work itself.
  * 6. Trademarks
- *    This license does not grant any rights to use any Licensor's or its 
- *    affiliates' names, logos, or trademarks, except as necessary to 
+ *    This license does not grant any rights to use any Licensor's or its
+ *    affiliates' names, logos, or trademarks, except as necessary to
  *    reproduce the notices described in this license.
  * 7. Limitations
- *    Platform. The Work and any derivative works thereof may only be used, or 
+ *    Platform. The Work and any derivative works thereof may only be used, or
  *    intended for use, with a Broadcom switch integrated circuit.
- *    No Reverse Engineering. You will not use the Work to disassemble, 
- *    reverse engineer, decompile, or attempt to ascertain the underlying 
+ *    No Reverse Engineering. You will not use the Work to disassemble,
+ *    reverse engineer, decompile, or attempt to ascertain the underlying
  *    technology of a Broadcom switch integrated circuit.
  * 8. Termination
- *    If you violate any term of this license, then your rights under this 
- *    license (including the license grants of Sections 2 and 3) will 
+ *    If you violate any term of this license, then your rights under this
+ *    license (including the license grants of Sections 2 and 3) will
  *    terminate immediately.
  * 9. Disclaimer of Warranty
- *    THE WORK IS PROVIDED "AS IS" WITHOUT WARRANTIES OR CONDITIONS OF ANY 
- *    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WARRANTIES OR CONDITIONS OF 
- *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE OR 
- *    NON-INFRINGEMENT. YOU BEAR THE RISK OF UNDERTAKING ANY ACTIVITIES UNDER 
- *    THIS LICENSE. SOME STATES' CONSUMER LAWS DO NOT ALLOW EXCLUSION OF AN 
+ *    THE WORK IS PROVIDED "AS IS" WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WARRANTIES OR CONDITIONS OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE OR
+ *    NON-INFRINGEMENT. YOU BEAR THE RISK OF UNDERTAKING ANY ACTIVITIES UNDER
+ *    THIS LICENSE. SOME STATES' CONSUMER LAWS DO NOT ALLOW EXCLUSION OF AN
  *    IMPLIED WARRANTY, SO THIS DISCLAIMER MAY NOT APPLY TO YOU.
  * 10. Limitation of Liability
- *    EXCEPT AS PROHIBITED BY APPLICABLE LAW, IN NO EVENT AND UNDER NO LEGAL 
- *    THEORY, WHETHER IN TORT (INCLUDING NEGLIGENCE), CONTRACT, OR OTHERWISE 
- *    SHALL ANY LICENSOR BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY DIRECT, 
- *    INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF 
- *    OR RELATED TO THIS LICENSE, THE USE OR INABILITY TO USE THE WORK 
- *    (INCLUDING BUT NOT LIMITED TO LOSS OF GOODWILL, BUSINESS INTERRUPTION, 
- *    LOST PROFITS OR DATA, COMPUTER FAILURE OR MALFUNCTION, OR ANY OTHER 
- *    COMMERCIAL DAMAGES OR LOSSES), EVEN IF THE LICENSOR HAS BEEN ADVISED OF 
+ *    EXCEPT AS PROHIBITED BY APPLICABLE LAW, IN NO EVENT AND UNDER NO LEGAL
+ *    THEORY, WHETHER IN TORT (INCLUDING NEGLIGENCE), CONTRACT, OR OTHERWISE
+ *    SHALL ANY LICENSOR BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY DIRECT,
+ *    INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF
+ *    OR RELATED TO THIS LICENSE, THE USE OR INABILITY TO USE THE WORK
+ *    (INCLUDING BUT NOT LIMITED TO LOSS OF GOODWILL, BUSINESS INTERRUPTION,
+ *    LOST PROFITS OR DATA, COMPUTER FAILURE OR MALFUNCTION, OR ANY OTHER
+ *    COMMERCIAL DAMAGES OR LOSSES), EVEN IF THE LICENSOR HAS BEEN ADVISED OF
  *    THE POSSIBILITY OF SUCH DAMAGES.
  */
 
@@ -428,6 +428,7 @@ ngknet_rx_pkt_filter(struct ngknet_dev *dev, struct sk_buff *skb, struct net_dev
     return SHR_E_NONE;
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
 static void
 ngknet_rl_process(unsigned long data)
 {
@@ -450,17 +451,45 @@ ngknet_rl_process(unsigned long data)
     rc->timer.expires = jiffies + HZ / rc->rx_ticks;
     add_timer(&rc->timer);
 }
+#else
+static void
+ngknet_rl_process(struct timer_list *t)
+{
+    struct ngknet_rl_ctrl *rc = from_timer(rc, t, timer);
+    struct ngknet_dev *dev;
+    unsigned long flags;
+    int idx;
+
+    spin_lock_irqsave(&rc->lock, flags);
+    rc->rx_pkts = 0;
+    for (idx = 0; idx < NUM_PDMA_DEV_MAX; idx++) {
+        dev = &rc->devs[idx];
+        if (rc->dev_active[idx] && rc->dev_paused[idx]) {
+            bcmcnet_pdma_dev_rx_resume(&dev->pdma_dev);
+            rl_ctrl.dev_paused[dev->dev_no] = 0;
+        }
+    }
+    spin_unlock_irqrestore(&rc->lock, flags);
+
+    rc->timer.expires = jiffies + HZ / rc->rx_ticks;
+    add_timer(&rc->timer);
+}
+#endif
 
 void
 ngknet_rx_rate_limit_init(struct ngknet_dev *devs)
 {
     sal_memset(&rl_ctrl, 0, sizeof(rl_ctrl));
     rl_ctrl.rx_ticks = 10;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,14,0)
     init_timer(&rl_ctrl.timer);
     rl_ctrl.timer.data = (unsigned long)&rl_ctrl;
     rl_ctrl.timer.function = ngknet_rl_process;
     spin_lock_init(&rl_ctrl.lock);
     rl_ctrl.devs = devs;
+#else
+    timer_setup(&rl_ctrl.timer, ngknet_rl_process, 0);
+#endif
 }
 
 void
